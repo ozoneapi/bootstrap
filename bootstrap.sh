e6cae962c1eb3442d7147e4cae2f5ffad57a2d0b
Navneet Karnani <navneet@ozoneapi.com>
2021-10-04 17:13:14 +0530

    handle creds better

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,36 +16,54 @@ fi
 echo " - running as user ${USER}. Check successful."
 echo " - Assuming user has \"sudo\" permissions with no need for password"
 
-if [[ `git config --global credential.helper | wc -l` = 0 ]]; then 
-  if [[ -f ~/.git-credentials ]]; then
-    HAVE_CREDS=$(cat ~/.git-credentials | grep -v bitbucket.org/ozoneapi | wc -l)
-  fi
-
-  if [[ ! -z $HAVE_CREDS && $HAVE_CREDS != 0 ]]; then
-    echo "git https creds configured."
-  elif [[ -v GIT_HTTPS_CREDS ]]; then
-    echo "persisting git https creds"
-    echo "https://$GIT_HTTPS_CREDS@bitbucket.org/ozoneapi" >> ~/.git-credentials
-  else
-    echo "Export the git https credentials before running this script. Run:"
-    echo "export GIT_HTTPS_CREDS=<username:app-password>"
-    exit -1
-  fi
-fi
-
 # ensure git is installed
 if [[ ! -f /usr/bin/git ]]; then
   echo "- git is not installed on this system. Installing git..."
   sudo yum install -y git-core
-  git config --global credential.helper store
+  # check for status again
+  if [[ -f /usr/bin/git ]]; then
+    echo "- git install check successful."
+  else
+    echo "- git install check failed. Ensure it is installed correctly."
+    exit -1
+  fi
 fi
 
-# check for status again
-if [[ -f /usr/bin/git ]]; then
-  echo "- git install check successful."
+# ensure git credential helper is configured to be the "store"
+if [[ `git config --global credential.helper | wc -l` = 0 ]]; then
+  echo "Credential Helper not configured. Configuring 'store'"
+  git config --global credential.helper store
+
+  # cross check the store has been configured, otherwise fail
+  if [[ `git config --global credential.helper | wc -l` = 0 ]]; then
+    >&2 echo "git store configuration failed. Cannot proceed."
+    exit -1
+  fi
+fi
+
+if [[ `git config --global credential.helper` != 'store' ]]; then 
+  echo "Credential helper is not 'store'. Skip git-credential configuration and check."
 else
-  echo "- git install check failed. Ensure it is installed correctly."
-  exit -1
+  echo 'Credential helper is "store". Validating ~/.git-credentials file.'
+
+  # Configure the credentials if they are not already done
+  unset -v HAVE_CREDS
+  if [[ -f ~/.git-credentials ]]; then
+    HAVE_CREDS=$(cat ~/.git-credentials | grep -v bitbucket.org/ozoneapi | wc -l)
+  fi
+
+  if [[ ! -z ${HAVE_CREDS} && ${HAVE_CREDS} != 0 ]]; then
+    echo "git https creds configured."
+  else 
+    # if credentials not configured, use defaults
+    if [[ ! -v GIT_HTTPS_CREDS ]]; then
+      echo "Export the git https credentials before running this script. Run:"
+      echo "export GIT_HTTPS_CREDS=<username:app-password>"
+      exit -1
+    fi
+    echo "persisting git https creds"
+    echo "https://$GIT_HTTPS_CREDS@bitbucket.org/ozoneapi" >> ~/.git-credentials
+  fi
 fi
 
 OZONE_HOME="/usr/o3"
@@ -53,7 +71,7 @@ GEPPETTO_HOME=${OZONE_HOME}/geppetto
 
 if [[ -d ${GEPPETTO_HOME} ]]; then
   echo "- Cleaning old ${GEPPETTO_HOME}"
-  sudo rm -rf /usr/o3/geppetto
+  sudo rm -rf ${GEPPETTO_HOME}
 fi
 
 echo "- Creating ${GEPPETTO_HOME}"
