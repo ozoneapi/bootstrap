68d0b7c444a668acf44a028057eccd6b617dacc6
Freddi Gyara <freddi@ozoneapi.com>
2023-05-30 23:27:50 +0000

    boostrap

--- docker-run/run-container.sh
+++ docker-run/run-container.sh
@@ -24,19 +24,19 @@ echo "Installing and starting docker - done"
 
 # login to docker repository
 echo "Logging in to docker repository - start"
-DOCKER_LOGIN_USERNAME=$(aws ssm get-parameter --name "docker-login-username" --region ${REGION} --query "Parameter.Value" --output text)
+DOCKER_LOGIN_USERNAME=$(getSsmParameter "docker-login-username")
 if [[ -z ${DOCKER_LOGIN_USERNAME} ]]; then
   echo "docker-login-username parameter not found in SSM. Cannot proceed."
   exit -1
 fi
 
-DOCKER_LOGIN_PASSWORD=$(aws ssm get-parameter --name "docker-login-password" --region ${REGION} --query "Parameter.Value" --output text)
+DOCKER_LOGIN_PASSWORD=$(getSsmParameter "docker-login-password")
 if [[ -z ${DOCKER_LOGIN_PASSWORD} ]]; then
   echo "docker-login-password parameter not found in SSM. Cannot proceed."
   exit -1
 fi
 
-DOCKER_LOGIN_REGISTRY=$(aws ssm get-parameter --name "docker-login-registry" --region ${REGION} --query "Parameter.Value" --output text)
+DOCKER_LOGIN_REGISTRY=$(getSsmParameter "docker-login-registry")
 if [[ -z ${DOCKER_LOGIN_REGISTRY} ]]; then
   echo "docker-login-registry parameter not found in SSM. Cannot proceed."
   exit -1
@@ -49,11 +49,15 @@ if [[ $? != 0 ]]; then
 fi
 
 # all set to run
-B64_DOCKER_RUN_COMMAND=$(getTag "DockerRunCommand")
+STACK_NAME=$(getTag "StackName")
+echo "getting stack name ${STACK_NAME}"
+
+B64_DOCKER_RUN_COMMAND=$(getSsmParameter "${STACK_NAME}.DockerRunCommand")
 
 DOCKER_RUN_COMMAND=$(echo ${B64_DOCKER_RUN_COMMAND} | base64 -d)
 
 echo "Running ${DOCKER_RUN_COMMAND} - start"
+
 docker run ${DOCKER_RUN_COMMAND}
 if [[ $? != 0 ]]; then
   >&2 echo "Error while running docker container."
