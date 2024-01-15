b0845db5cf28fde6b2d212c18cc53fa8e23cdd41
Stanislav Scaletchi <stanislav.scaletchi@ozoneapi.com>
2024-01-15 06:56:42 +0000

    feature(PLAT-105): changed if condition syntax

--- ssm-bootstrap-observability.sh
+++ ssm-bootstrap-observability.sh
@@ -23,12 +23,14 @@ git config --global credential.https://bitbucket.org.useHttpPath true
 
 git config --global credential.helper '!f() {
   sleep 1
-  if [[ -z ${GIT_HTTPS_CREDS} ]]; then
+  if [ -z ${GIT_HTTPS_CREDS} ]]
+  then
     export TOKEN=$(curl --max-time 0.5 -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 2")
     export REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region -H "X-aws-ec2-metadata-token: $TOKEN")
     export GIT_HTTPS_CREDS=$(aws ssm get-parameter --name git.https.creds --region ${REGION} --with-decryption --query Parameter.Value --output text)
   fi
-  if [[ -n $GIT_HTTPS_CREDS ]]; then
+  if [ -n $GIT_HTTPS_CREDS ]
+  then
     local GIT_USERNAME=$(echo ${GIT_HTTPS_CREDS} | cut -d ':' -f1)
     local GIT_ACCESS_CRED=$(echo ${GIT_HTTPS_CREDS} | cut -d ':' -f2)
     echo "username=${GIT_USERNAME}"
@@ -46,12 +48,14 @@ fi
 
 git config --global credential.https://bitbucket.org/ozoneapi.helper '!f() {
   sleep 1
-  if [[ -z ${GIT_HTTPS_CREDS} ]]; then
+  if [ -z ${GIT_HTTPS_CREDS} ]
+  then
     export TOKEN=$(curl --max-time 0.5 -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 2")
     export REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region -H "X-aws-ec2-metadata-token: $TOKEN")
     export GIT_HTTPS_CREDS=$(aws ssm get-parameter --name git.https.creds --region ${REGION} --with-decryption --query Parameter.Value --output text)
   fi
-  if [[ -n $GIT_HTTPS_CREDS ]]; then
+  if [ -n $GIT_HTTPS_CREDS ]
+  then
     local GIT_USERNAME=$(echo ${GIT_HTTPS_CREDS} | cut -d ':' -f1)
     local GIT_ACCESS_CRED=$(echo ${GIT_HTTPS_CREDS} | cut -d ':' -f2)
     echo "username=${GIT_USERNAME}"
