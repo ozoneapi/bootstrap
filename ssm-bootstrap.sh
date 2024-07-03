878325ad496272c87fd2e92e2230909f002da8ee
Stan Scaletchi <163834701+OzoneStanScaletchi@users.noreply.github.com>
2024-07-03 18:20:36 +0300

    updated credentials helper (#2)

--- ssm-bootstrap.sh
+++ ssm-bootstrap.sh
@@ -29,12 +29,6 @@ git config --global credential.helper '!f() {
     export REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region -H "X-aws-ec2-metadata-token: $TOKEN")
     export GITHUB_HTTPS_CREDS=$(aws ssm get-parameter --name github.https.creds --region ${REGION} --with-decryption --query Parameter.Value --output text)
   fi
-  if [[ -n $GITHUB_HTTPS_CREDS ]]; then
-    local GIT_USERNAME=$(echo ${GITHUB_HTTPS_CREDS} | cut -d ':' -f1)
-    local GIT_ACCESS_CRED=$(echo ${GITHUB_HTTPS_CREDS} | cut -d ':' -f2)
-    echo "username=${GIT_USERNAME}"
-    echo "password=${GIT_ACCESS_CRED}"
-  fi
 }; f'
 
 
@@ -53,12 +47,6 @@ git config --global credential.https://github.com/ozoneapi.helper '!f() {
     export TOKEN=$(curl --max-time 0.5 -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 2")
     export REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region -H "X-aws-ec2-metadata-token: $TOKEN")
     export GITHUB_HTTPS_CREDS=$(aws ssm get-parameter --name github.https.creds --region ${REGION} --with-decryption --query Parameter.Value --output text)
-  fi
-  if [[ -n $GITHUB_HTTPS_CREDS ]]; then
-    local GIT_USERNAME=$(echo ${GITHUB_HTTPS_CREDS} | cut -d ':' -f1)
-    local GIT_ACCESS_CRED=$(echo ${GITHUB_HTTPS_CREDS} | cut -d ':' -f2)
-    echo "username=${GIT_USERNAME}"
-    echo "password=${GIT_ACCESS_CRED}"
   fi
 }; f'
 
